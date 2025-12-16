23759833dc76a6883f02773f62ccf2d89e87b970
Carota <giacomo.dinoto@unimore.it>
2025-12-16 11:51:52 +0100

    Update version from 0.4.4 to 0.4.5

--- RUN_USMA_PORTABLE.bat
+++ RUN_USMA_PORTABLE.bat
@@ -1,10 +1,10 @@
 @echo off
-TITLE USMA v0.4.4 - Portable Launcher
+TITLE USMA v0.4.5 - Portable Launcher
 
 cd /d "%~dp0"
 
 echo ========================================
-echo USMA v0.4.4 - Portable Edition
+echo USMA v0.4.5 - Portable Edition
 echo ========================================
 echo.
 
